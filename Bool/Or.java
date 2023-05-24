39a73b31d1e643e99e28e9d696b1304af8f3b38b
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-05-24 19:45:55 +0300

    Create Or.java

---
+++ Bool/Or.java
@@ -0,0 +1,28 @@
+package Bool;
+
+public class Or implements CommonExpression {
+
+    private CommonExpression l, r;
+    
+    public Or(CommonExpression l, CommonExpression r) {
+        this.l = l;
+        this.r = r;
+    }
+    @Override
+    public boolean evaluate(boolean... args) {
+        return Or(l.evaluate(args), r.evaluate(args));
+    }
+
+    protected boolean Or(boolean a, boolean b) {
+        return (a == true) || (b == true);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + l + "|" + r + ")";
+    }
+
+    public char get() {
+        return ' ';
+    }
+}

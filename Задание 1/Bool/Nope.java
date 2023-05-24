5eed033d1eb8bdd7fd4a80d1e3977b2c5c9afa80
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-05-24 20:15:56 +0300

    Create Nope.java

---
+++ Задание 1/Bool/Nope.java
@@ -0,0 +1,21 @@
+package Bool;
+
+public class Nope implements CommonExpression {
+    private char c;
+    public Nope(char c) {
+        this.c = c;
+    }
+
+    public char get() {
+        return c;
+    }
+    @Override
+    public boolean evaluate(boolean... args) {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "" + c;
+    }
+}

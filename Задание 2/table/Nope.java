facad343bf36a6f80bf269eca714ded3fd8d5900
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-06-05 18:01:30 +0300

    Update Nope.java

--- Задание 2/table/Nope.java
+++ Задание 2/table/Nope.java
@@ -1,1 +1,21 @@
+package table;
 
+public class Nope implements CommonExpression {
+    private char c;
+    public Nope(char c) {
+        this.c = c;
+    }
+
+    public char get() {
+        return c;
+    }
+    @Override
+    public double evaluate(double... args) {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "" + c;
+    }
+}

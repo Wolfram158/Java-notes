d22e633318ce9b7d56b8e8eda378d310dc570d88
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-07-30 18:29:40 +0300

    Update Player.java

--- Sticks/sticks/Player.java
+++ Sticks/sticks/Player.java
@@ -1,1 +1,10 @@
+package sticks;
 
+public interface Player {
+    String getSign();
+    int[] makeMove(Board board);
+    int getScore();
+    void setScore(int value);
+    void incScore();
+    String getName();
+}
